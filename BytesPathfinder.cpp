d97ae609c05ba8b3d142a70a6ac18055cc1eaf67
ItamiOfficial <57113216+ItamiOfficial@users.noreply.github.com>
2024-09-19 21:18:52 +0200

    Initial Commit.
    A* and Dijkstra work. Use a Priority Queue (Heap). 
    
    A* Currently only works for Nodes, not for Grids. (Dijkstra does not use a Heuristic, thus every grid is the same for it. Memory could however get more performant, as you dont need an Adjacency Matrix and could just calculate the indices on the fly)

---
+++ BytesPathfinder.cpp
@@ -0,0 +1,356 @@
+﻿// Copyright by BytesOfProcrastination all rights reserved CC 2024
+
+#include "Pathfinding/BytesPathfinder.h"
+
+#include "Utility/StopWatch.h"
+
+constexpr int32 INITIAL_DISTANCE = 2000000;
+
+void UBytesPathfinder::FindPathsToNodes(FBytesGraph& Graph, const int32 StartID)
+{
+	/* Declare "Unvisited" TSet which contains ID's of unvisited Nodes
+	*  easier to remove Nodes, but should be a sorted queue ideally,
+	*  so we do not have to iterate over the hole Set to find
+	*  the Node with the lowest GCost
+	*/
+
+	// StopWatch
+	FStopWatch StopWatch;
+	
+	// This has to be a reference. When Nodes in the Grid get changed, they wont get changed inside the MinHeap
+	// And because they are not changed, this thing does not work.
+	FBytesPathfindingHeap Unvisited = FBytesPathfindingHeap(Graph.Nodes.Num());
+	
+	InitNodes(Graph);
+	StopWatch.Measure("Post Graph Init");
+	
+	// Set Starting Node to 0
+	Graph.Nodes[StartID].GCost = 0;
+
+	// Add Nodes to Unvisited
+	for (FBytesNode& Node : Graph.Nodes)
+	{
+		// Add Node to Unvisited Array
+		Unvisited.Add(&Node);
+	}
+
+	// Remove Late Print
+	// Unvisited.LogHeap();
+	// Unvisited.UpdateItem(&Graph.Nodes[StartNodeID]);
+
+	// As long as there are unvisited Tiles we keep searching
+	while (Unvisited.IsNotEmpty())
+	{
+		// Find closest and  remove current
+		const auto Node = Unvisited.RemoveFirst();
+		
+		// Was used before: "FindNodeWithLowestGCost(Graph, Unvisited);"
+		// Was Used before: "Unvisited.RemoveSingle(CurrentNodeID);"
+
+		for (const auto& NeighbourEdge : Graph.Edges[Node->NodeID].NeighbouringEdges)
+		{
+			// Calculate GCost to Neighbour, which is CurrentNode's GCost + the Edge Weight
+			const int32 Distance = Graph.Nodes[Node->NodeID].GCost + NeighbourEdge.Weight;
+
+			// if Distance is closer, update Parent and GCost
+			if (Distance < Graph.Nodes[NeighbourEdge.NodeID].GCost)
+			{
+				Graph.Nodes[NeighbourEdge.NodeID].GCost = Distance;
+				Graph.Nodes[NeighbourEdge.NodeID].ParentID = Node->NodeID;
+
+				// Update Entry
+				Unvisited.UpdateItem(&Graph.Nodes[NeighbourEdge.NodeID]);
+			}
+		}
+		
+		// Remove Late Print
+		// Unvisited.LogHeap();
+	}
+
+	StopWatch.Measure("Finished Djikstra with Heap");
+	StopWatch.Log();
+}
+
+void UBytesPathfinder::FindPath(FBytesGraph& Graph, const int32 StartID, const int32 TargetID)
+{
+	// Declare StopWatch and Start its Timer
+	FStopWatch StopWatch;
+
+	// Inits Nodes
+	InitNodes(Graph);
+
+	// Stop Time
+	StopWatch.Measure("Post Init Nodes");
+	
+	// Declare "Open List" and "Closed List"
+	FBytesPathfindingHeap OpenSet = FBytesPathfindingHeap(Graph.Nodes.Num());
+	TSet<int32> ClosedSet;
+
+	// Add First Node
+	Graph.Nodes[StartID].GCost = 0;
+	Graph.Nodes[StartID].HCost = CalcHeuristicDistance(Graph, StartID, TargetID);
+	OpenSet.Add(&Graph.Nodes[StartID]);
+
+	while(OpenSet.IsNotEmpty())
+	{
+		// Get Current Node
+		auto CurrentNode = OpenSet.RemoveFirst();
+
+		// Add Current Node to Closed List
+		ClosedSet.Add(CurrentNode->NodeID);
+
+		// Check if Current is Target
+		if (CurrentNode->NodeID == TargetID)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Pathfinding: Path Found"));
+
+			// Stop Time
+			StopWatch.Measure("Post Path Found");
+			StopWatch.Log();
+			return;
+		}
+		
+		// Iterate over Neighbours
+		for (const auto& Edge : Graph.Edges[CurrentNode->NodeID].NeighbouringEdges)
+		{
+			// Neighbour
+			const auto Neighbour = &Graph.Nodes[Edge.NodeID];
+			
+			// if Closed List Contains Neighbour, return
+			if (ClosedSet.Contains(Neighbour->NodeID))
+			{
+				continue;
+			}
+
+			// Calculate Cost to Neighbour
+			int32 MovementCost = CurrentNode->GCost + Edge.Weight;
+
+			// if Neighbour is in OpenList AND has higher GCost return
+			if (OpenSet.Contains(Neighbour) &&  Neighbour->GCost < MovementCost)
+			{
+				continue;
+			}
+
+			// Set Current Node Parent
+			Neighbour->ParentID = CurrentNode->NodeID;
+
+			// Set New Movement Cost
+			Neighbour->GCost = MovementCost;
+			Neighbour->HCost = CalcHeuristicDistance(Graph, Neighbour->NodeID, TargetID);
+
+			if (OpenSet.Contains(Neighbour))
+			{
+				OpenSet.UpdateItem(Neighbour);
+			} else
+			{
+				OpenSet.Add(Neighbour);
+			}
+		}
+		
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Pathfinding: No Path Found"));
+	StopWatch.Measure("Post No Path Found");
+	StopWatch.Log();
+}
+
+TArray<int32> UBytesPathfinder::GetNodesInRange(FBytesGraph& Graph, const int32 MaxTravelCost)
+{
+	TArray<int32> ReturnArray;
+
+	for (const auto& Node : Graph.Nodes)
+	{
+		if(Node.GCost <= MaxTravelCost) {
+			ReturnArray.Add(Node.NodeID);
+		}
+	}
+
+	return ReturnArray;
+}
+
+TArray<int32> UBytesPathfinder::GetPath(FBytesGraph& Graph, const int32 StartNodeID, const int32 TargetNodeID, const bool bRecalculate)
+{
+	TArray<int32> Path;
+
+	// Check if Indices are valid
+	if (!Graph.Nodes.IsValidIndex(StartNodeID) || !Graph.Nodes.IsValidIndex(TargetNodeID))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Pathfinding: Invalid Node ID's. Out of Range"));
+		return Path;
+	}
+	
+	// Check if Target Node has ever been reached
+	if (Graph.Nodes[TargetNodeID].ParentID == -1)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Pathfinding: Target Node has never been Reached"));
+		return Path;
+	}
+
+	// if we should recalculate the path, we use A*
+	// It Could be that we had a Dijkstra before and cached it, so we dont need to recalc
+	if (bRecalculate)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Pathfinding: Path Found Using A*. Heuristics still SUCK!"));
+
+		FindPath(Graph, StartNodeID, TargetNodeID);
+	}
+	
+	// Retrace Path
+	int32 CurrentNodeID = TargetNodeID;
+
+	// Iterate over each parent of current node until we reached Start Node
+	// We Iterate from Target -> Start
+	while(CurrentNodeID != StartNodeID)
+	{
+		// Add Current Node to Path
+		Path.Add(CurrentNodeID);
+
+		// Set Current Node to Parent ID of current Node
+		CurrentNodeID = Graph.Nodes[CurrentNodeID].ParentID;
+	}
+
+	// Reverse TArray
+	Algo::Reverse(Path);
+	
+	return Path;
+}
+
+FBytesGraph UBytesPathfinder::CreateGraph()
+{
+	return FBytesGraph();
+}
+
+int32 UBytesPathfinder::AddNode(FBytesGraph& Graph, FVector2D Location2D)
+{
+	// ==== Sub Section | Node ==== //
+	
+	// Create new Node
+	FBytesNode NewNode;
+
+	// Set NodeID to MaxIndex
+	NewNode.NodeID = Graph.Nodes.Num();
+
+	// Add Node to Graph
+	Graph.Nodes.Add(NewNode);
+
+	// Set Location 2D
+	NewNode.Location2D = Location2D;
+
+	// ==== Sub Section | Edges ==== //
+
+	// Create new Edges (Container for multiple "FBytesEdge" Structs)
+	FBytesEdges NewEdges;
+
+	// Add Edges Container, right after Node gets Created
+	Graph.Edges.Add(NewEdges);
+	
+	// Return ID
+	return NewNode.NodeID;
+}
+
+void UBytesPathfinder::AddOrSetEdge(FBytesGraph& Graph, const int32 NodeAID, const int32 NodeBID, const int32 Weight)
+	{
+	// Check if the Nodes even Exist
+	if (!Graph.Nodes.IsValidIndex(NodeAID) || !Graph.Nodes.IsValidIndex(NodeBID))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Error, one of the Vertices is not in Graph"));
+		return;
+	}
+
+	// Checks if the Edges already exist, and only overwrite their Weight
+	if (Graph.Edges[NodeAID].NeighbouringEdges.ContainsByPredicate([NodeBID] (const FBytesEdge Edge)
+	{
+		return Edge.NodeID == NodeBID;
+	}))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Edge Weight gets overridden"));
+
+		// Change Edge A
+		for (auto& Edge : Graph.Edges[NodeAID].NeighbouringEdges)
+		{
+			if (Edge.NodeID == NodeBID)
+			{
+				Edge.Weight = Weight;
+			}
+		}
+
+		// Change Edge B
+		for (auto& Edge : Graph.Edges[NodeBID].NeighbouringEdges)
+		{
+			if (Edge.NodeID == NodeAID)
+			{
+				Edge.Weight = Weight;
+			}
+		}
+
+		// Return Early
+		return;
+	}
+
+	
+	// Create Structs
+	FBytesEdge EdgeAB;
+	FBytesEdge EdgeBA;
+
+	// Set Edge A -> B
+	EdgeAB.Weight = Weight;
+	EdgeAB.NodeID = NodeBID;
+
+	// Set Edge B -> A
+	EdgeBA.Weight = Weight;
+	EdgeBA.NodeID = NodeAID;
+
+	// Sets Elements
+	Graph.Edges[NodeAID].NeighbouringEdges.Add(EdgeAB);
+	Graph.Edges[NodeBID].NeighbouringEdges.Add(EdgeBA);
+
+}
+
+int32 UBytesPathfinder::FindNodeWithLowestGCost(const FBytesGraph& Graph, const TArray<int32>& Unvisited)
+{
+	// Closest is random, but negative one will never be set, so there cant be any problems
+	int32 Closest = Unvisited[0];
+	// the "- 1" is very important, as it will loop forever if there are no edges to a Point.
+	// So there is "normally" no problem, but for my debugging adventures this sucks...
+	int32 CurrentCost = Graph.Nodes[Closest].GCost;
+
+	for (const auto NodeID : Unvisited)
+	{
+		// Get Node
+		const FBytesNode& Node = Graph.Nodes[NodeID];
+
+		// Check if Node has lower than current
+		if (Node.GCost < CurrentCost)
+		{
+			Closest = NodeID;
+			CurrentCost = Node.GCost;
+		}
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Cost: %d"), CurrentCost);
+	
+	return Closest;
+}
+
+int32 UBytesPathfinder::CalcHeuristicDistance(const FBytesGraph& Graph, const int32 StartID, const int32 TargetID)
+{
+	auto NodeA = &Graph.Nodes[StartID];
+	auto NodeB = &Graph.Nodes[TargetID];
+
+	return FMath::FloorToInt32(FVector2D::Distance(NodeA->Location2D, NodeB->Location2D));
+}
+
+void UBytesPathfinder::InitNodes(FBytesGraph& Graph)
+{
+	// Init Node
+	for (FBytesNode& Node : Graph.Nodes)
+	{
+		// Set GCosCostt to Max
+		Node.GCost = INITIAL_DISTANCE;
+		Node.HCost = 0;
+
+		// Set Parent to "none" represented by -1.
+		// This is Important for checking if there even is a path to Target!
+		Node.ParentID = -1;
+	}
+}
---
+++ BytesPathfinder.h
@@ -0,0 +1,313 @@
+﻿// Copyright by BytesOfProcrastination all rights reserved CC 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include <vector>
+#include "BytesPathfinder.generated.h"
+
+UENUM(BlueprintType)
+enum class EBytesGraphType : uint8
+{
+	Distance2D,
+	Square,
+	Hexagonal,
+};
+
+// ==== Section | Pathfinder Utility Classes/Structs ==== //
+
+// Simple Node with G & H Cost, Parent ID and a Node ID for referencing itself and Objects outside the Pathfinder
+USTRUCT(BlueprintType)
+struct FBytesNode
+{
+	GENERATED_BODY()
+
+	// Node ID for referencing it back to request
+	UPROPERTY()
+	int32 NodeID = -1;
+
+	UPROPERTY()
+	int32 ParentID = -1;
+	
+	// Cost it took to get here
+	UPROPERTY()
+	int32 GCost = 0;
+
+	// Heuristic guess about distance to Target
+	UPROPERTY()
+	int32 HCost = 0;
+
+	// Needed for Calculation of Heuristic
+	UPROPERTY()
+	FVector2D Location2D;
+
+	// Index important for Heap
+	UPROPERTY()
+	int32 HeapIndex;
+
+	// A* Only, combines GCost + HCost
+	int32 FCost() const
+	{
+		return GCost + HCost;
+	}
+};
+
+// A Non-Templated Heap for the Pathfinding
+class FBytesPathfindingHeap
+{
+public:
+	explicit FBytesPathfindingHeap(const int32 Capacity)
+	{
+		// Init Vector with Size
+		Items.resize(Capacity);
+
+		// Init Item Count
+		Size = 0;
+	}
+
+	void Add(FBytesNode* Node)
+	{
+		Node->HeapIndex = Size;
+		Items[Size] = Node;
+		SortUp(Node);
+		Size++;
+	}
+
+	FBytesNode* RemoveFirst()
+	{
+		auto First = Items[0];
+		Size--;
+		Items[0] = Items[Size];
+		Items[0]->HeapIndex = 0;
+
+		SortDown(Items[0]);
+		return First;
+	}
+
+	// In a Heap for Pathfinding, we only need to sort up, as they only get changed, when they
+	// become better
+	void UpdateItem(FBytesNode* Node)
+	{
+		SortUp(Node);
+	}
+
+	bool IsNotEmpty() const
+	{
+		return Size > 0;
+	}
+
+	bool Contains(const FBytesNode* Node) const
+	{
+		for (auto Elem : Items)
+		{
+			if (Node == Elem)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	void LogHeap() const
+	{
+		for (int i = 0; i < Size; i++)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Heap Position: %d | Distance: %d | ID: %d"), i, Items[i]->FCost(), Items[i]->NodeID);
+		}
+	}
+	
+private:
+	// Container 
+	std::vector<FBytesNode*> Items;
+
+	// Current Size
+	int32 Size;
+
+	// Parent Shortcut
+	int GetParent (const int32 Index) const { return (Index - 1) / 2; }
+	int GetLeftChild (const int32 Index) const { return Index * 2 + 1; }
+	int GetRightChild (const int32 Index) const { return Index * 2 + 2; }
+
+	// Sorting
+	void SortUp(FBytesNode* Node) 
+	{
+		// Get Parent
+		FBytesNode* Parent = Items[GetParent(Node->HeapIndex)];
+
+		// When the Child has a higher priority than the parent, we swap them
+		if (Node->FCost() < Parent->FCost() || Node->FCost() == Parent->FCost() && Node->HCost < Parent->HCost)
+		{
+			// Switch Positions
+			Swap(Parent, Node);
+
+			// Recursive calling until Node is high enough
+			SortUp(Node);
+		}
+	}
+
+	void SortDown(FBytesNode* Node)
+	{
+		while (true)
+		{
+			auto LIndex = GetLeftChild(Node->HeapIndex);
+			auto RIndex = GetRightChild(Node->HeapIndex);
+			auto SwapIndex = 0;
+			
+			if (LIndex < Size)
+			{
+				SwapIndex = LIndex;
+
+				if(RIndex < Size)
+				{
+					// Check if we switch with left or right child
+					if (Items[LIndex]->FCost() > Items[RIndex]->FCost() || Items[LIndex]->FCost() == Items[RIndex]->FCost() && Items[LIndex]->HCost > Items[RIndex]->HCost)
+					{
+						SwapIndex = RIndex;
+					}
+				}
+
+				// Check if we swap at all
+				auto ItemToSwap = Items[SwapIndex];
+				if (Node->FCost() > ItemToSwap->FCost() || Node->FCost() == ItemToSwap->FCost() && Node->HCost > ItemToSwap->HCost)
+				{
+					Swap(Node, Items[SwapIndex]);
+				}
+				else
+				{
+					return;
+				}
+			}
+			else
+			{
+				return;
+			}
+		}
+	}
+
+	void Swap(FBytesNode* X, FBytesNode* Y)
+	{
+		// switch vector positions
+		Items[X->HeapIndex] = Y;
+		Items[Y->HeapIndex] = X;
+
+		int Temp = X->HeapIndex;
+
+		X->HeapIndex = Y->HeapIndex;
+		Y->HeapIndex = Temp;
+	}
+};
+
+// A Single Edge, containing End and Weight
+USTRUCT(BlueprintType)
+struct BYTESHEXGRIDPLUGIN_API FBytesEdge
+{
+	GENERATED_BODY()
+
+	// The End of this Edge Basically
+	UPROPERTY()
+	int32 NodeID;
+
+	// Travel Cost/Weight... basically what gets added to GCost...
+	UPROPERTY()
+	int32 Weight;
+};
+
+// A Container of Edges, forms inside a TArray an Adjacency Matrix
+USTRUCT()
+struct FBytesEdges
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TArray<FBytesEdge> NeighbouringEdges;
+};
+
+// ==== The Graph itself we perform our Pathfinding on ==== //
+USTRUCT(BlueprintType)
+struct BYTESHEXGRIDPLUGIN_API FBytesGraph
+{
+	GENERATED_BODY()
+	
+	UPROPERTY()
+	TArray<FBytesNode> Nodes;
+
+	UPROPERTY()
+	TArray<FBytesEdges> Edges;
+
+	UPROPERTY()
+	EBytesGraphType GraphType;
+};
+
+// ==== Section | Pathfinder BP Function Library ==== //
+UCLASS(BlueprintType, DefaultToInstanced)
+class BYTESHEXGRIDPLUGIN_API UBytesPathfinder : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	/*
+	 * Change Name...
+	 *
+	 * This Basically performs a Dijkstra on a Graph.
+	 * After this has been performed, you can use the "GetPath()" Method
+	 * to get a TArray<int32> which contains the NodeID's.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Pathfinder|Pathfinding")
+	static void FindPathsToNodes(UPARAM(ref) FBytesGraph& Graph, const int32 StartID);
+
+	UFUNCTION()
+	static void FindPath(FBytesGraph& Graph, const int32 StartID, const int32 TargetID);
+
+	/*
+	 * Only call after "Find Paths to Nodes
+	 * Returns all NodeID's of reachable Nodes.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Pathfinder|Pathfinding")
+	static TArray<int32> GetNodesInRange(UPARAM(ref) FBytesGraph& Graph, const int32 MaxTravelCost);
+
+	/*
+	 * StartNodeID: The ID of the starting Node
+	 * TargetNodeID: The ID of the targeted Node
+	 * bRecalculate: Performs A* if true, else uses Graph as is
+	 */
+	UFUNCTION(BlueprintCallable, Category="Pathfinder|Pathfinding")
+	static TArray<int32> GetPath(UPARAM(ref) FBytesGraph& Graph, const int32 StartNodeID, const int32 TargetNodeID, bool bRecalculate);
+
+	/*
+	 * Returns a new Graph
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Pathfinder|Graph")
+	static FBytesGraph CreateGraph();
+
+	/*
+	 * Creates a new "FBytesNode" and a "FBytesEdges"
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Pathfinder|Graph")
+	static int32 AddNode(UPARAM(ref) FBytesGraph& Graph, FVector2D Location2D);
+
+	/*
+	 * Creates a new Edge and adds them to their designated "FBytesEdges" 
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Pathfinder|Graph")
+	static void AddOrSetEdge(UPARAM(ref) FBytesGraph& Graph, const int32 NodeAID, const int32 NodeBID, const int32 Weight);
+	
+private:
+	// Linear Search Through an Array...
+	UFUNCTION()
+	static int32 FindNodeWithLowestGCost(const FBytesGraph& Graph, const TArray<int32>& Unvisited);
+
+	// Heuristic Calculations for 
+	UFUNCTION()
+	static int32 CalcHeuristicDistance(const FBytesGraph& Graph, const int32 StartID, const int32 TargetID);
+
+	UFUNCTION()
+	static void InitNodes(FBytesGraph& Graph);
+	
+};
+
+/*
+ * ToDo:
+ * - Add Different Versions of Dijkstar and A* for Square and Hex Grids (Because of Heuristic and )
+ * - Remove BlueprintType
+ */
